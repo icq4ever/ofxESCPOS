0117015abbc3e5d1e42061de4149d5cfef05d206
Yi donghoon <icq4ever@gmail.com>
2019-03-17 00:50:17 +0900

    paper status added

--- libs/ofxESCPOS/Commands/src/DefaultPrinterStatusCommands.cpp
+++ libs/ofxESCPOS/Commands/src/DefaultPrinterStatusCommands.cpp
@@ -82,9 +82,49 @@ std::size_t DefaultPrinterStatusCommands::getErrorStatus()
 }
 
 
-std::size_t DefaultPrinterStatusCommands::getPaperStatus()
-{
-    return 0; // TBI
+std::size_t DefaultPrinterStatusCommands::getPaperStatus(){
+    // return 0; // TBI
+    
+    // 0 : paper out
+    // 1 : paper ok
+    // 2 : reserved for "almost paper out" not sure working. so not used now.
+    uint8_t paperStatus;
+
+    const uint8_t command[3] = {
+        BaseCodes::DLE,
+        BaseCodes::EOT,
+        BaseCodes::STATUS_PAPER
+      };
+
+    std::size_t numBytesWritten = writeBytes(command, 3);
+
+    if (3 == numBytesWritten) {
+        uint8_t data;
+
+        // pSerial->waitByteTimes(3);
+        std::size_t numBytesRead = readByte(data);
+
+        if (numBytesRead > 0)    {
+            std::bitset<8> bits(data);
+            std::cout << "Data Read: " << bits << std::endl;
+
+
+            if(data == 114)         paperStatus = 0;
+            // not sure almost paper out working. so just paper OK except paper out
+            // else if (data == 18)    paperStatus = 2;
+            else                    paperStatus = 1;    
+
+            // package these
+
+        } else {
+            ofLogError("DefaultPrinterStatusCommands::getPrinterStatus") << "No bytes read.";
+        }
+
+    } else {
+        ofLogError("DefaultPrinterStatusCommands::getPrinterStatus") << "Number of bytes written was " << numBytesWritten;
+    }
+
+    return paperStatus;
 }
 
 
